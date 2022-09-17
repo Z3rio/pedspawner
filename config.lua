1dccd136fa6e33ffbe539cee1241025e7a762280
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-09-17 14:18:28 +0200

    Add some more scenarios

--- config.lua
+++ config.lua
@@ -7,5 +7,20 @@ Config.Animations = {
         text = "Binoculars",
         value = "binoculars", -- used in command
         scenario = "WORLD_HUMAN_BINOCULARS"
-    }
+    },
+    {
+        text = "Cop",
+        value = "cop",
+        scenario = "WORLD_HUMAN_COP_IDLES"
+    },
+    {
+        text = "Guard",
+        value = "guard",
+        scenario = "WORLD_HUMAN_GUARD_STAND"
+    },
+    {
+        text = "Smoke",
+        value = "smoke",
+        scenario = "WORLD_HUMAN_SMOKING"
+    }    
 }
