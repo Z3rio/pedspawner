cc8c39f4233bdd40a37e588b4be9d500e1251843
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-09-17 18:10:02 +0200

    Add ped follow player + ped vehicles

--- README.md
+++ README.md
@@ -3,11 +3,6 @@ This is pedspawner made to work with QB-Core.
 
 # Usage:
 Simply do /createped and it will bring up a menu to create a ped.
-You can also create peds directly through the command: 
-<br><br>
-/createped [model] [vehicleseat (leave -1 if not wanted)] [animation (has to be valid from config)] [invincible (true / false)] [freeze pos [true / false]]
-<br>
-**Example**: /createped a_f_m_bevhills_01 -1 binoculars true false
 
 # Other commands:
 /clearpeds - Removes all peds
--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-local Peds = {}
+local Peds, Vehs, FollowPlayerPeds = {}, {}, {}
 QBCore = exports['qb-core']:GetCoreObject()
 
 local animations = {}
@@ -9,122 +9,86 @@ for i,v in pairs(Config.Animations) do
     })
 end
 
-RegisterCommand("createped", function(source, commandArgs)
+RegisterCommand("createped", function()
     QBCore.Functions.TriggerCallback("qb-admin:server:getrank", function(result)
         if result then
             local coords = GetEntityCoords(PlayerPedId())
 
-            if not commandArgs or not commandArgs[1] then
-                local inputArgs = exports['qb-input']:ShowInput({
-                    header = "Create a ped",
-                    submitText = "Spawn",
-                    inputs = {
-                        {
-                            text = "Ped model",
-                            name = "model",
-                            type = "text",
-                            isRequired = true
-                        },
-                        {
-                            text = "Vehicle Seat Number",
-                            name = "vehseat",
-                            type = "text",
-                            isRequired = false
-                        },
-                        {
-                            text = "Animation",
-                            name = "anim",
-                            type = "select",
-                            options = animations,
-                            isRequired = false
-                        },
-                        {
-                            text = "Extra Attributes",
-                            name = "attributes",
-                            type = "checkbox",
-                            options = {
-                                {
-                                    text = "Freeze Position",
-                                    value = "freezepos"
-                                },
-                                {
-                                    text = "Invincible",
-                                    value = "invincible"
-                                }
+            local inputArgs = exports['qb-input']:ShowInput({
+                header = "Create a ped",
+                submitText = "Spawn",
+                inputs = {
+                    {
+                        text = "Ped model",
+                        name = "model",
+                        type = "text",
+                        isRequired = true
+                    },
+                    {
+                        text = "Vehicle model (not needed)",
+                        name = "vehmodel",
+                        type = "text",
+                        isRequired = false
+                    },
+                    {
+                        text = "Vehicle Seat Number (not needed)",
+                        name = "vehseat",
+                        type = "text",
+                        isRequired = false
+                    },
+                    {
+                        text = "Animation",
+                        name = "anim",
+                        type = "select",
+                        options = animations,
+                        isRequired = false
+                    },
+                    {
+                        text = "Extra Attributes",
+                        name = "attributes",
+                        type = "checkbox",
+                        options = {
+                            {
+                                text = "Freeze Position",
+                                value = "freezepos"
                             },
-                            isRequired = false
-                        }
+                            {
+                                text = "Invincible",
+                                value = "invincible"
+                            },
+                            {
+                                text = "Follow Player",
+                                value = "followplr"
+                            }
+                        },
+                        isRequired = false
                     }
-                })
-            
-                if inputArgs ~= nil then
-                    local hash = GetHashKey(tostring(inputArgs.model))
+                }
+            })
+        
+            if inputArgs ~= nil and inputArgs.model then
+                local hash = GetHashKey(tostring(inputArgs.model))
 
-                    local loadIdx = 0
-                    RequestModel(hash)
-                    while not HasModelLoaded(hash) do 
-                        Wait(100) 
-                        loadIdx = loadIdx + 1
-                        if loadIdx > 20 then
-                            QBCore.Functions.Notify("Couldn't load / find the ped model", "error")
-                            return
-                        end
-                    end
-
-                    local ped = CreatePed(4, hash, coords.x, coords.y, coords.z - 1, 0.0, true, true)
-
-                    SetEntityHeading(ped, GetEntityHeading(PlayerPedId()))
-
-                    if inputArgs.anim and Config.Animations[tonumber(inputArgs.anim)] and Config.Animations[tonumber(inputArgs.anim)].scenario then
-                        TaskStartScenarioInPlace(ped, Config.Animations[tonumber(inputArgs.anim)].scenario, 0, 1)
-                    end
-
-                    if inputArgs.invincible then
-                        SetBlockingOfNonTemporaryEvents(ped, true)
-                        SetPedDiesWhenInjured(ped, false)
-                        SetPedCanPlayAmbientAnims(ped, true)
-                        SetPedCanRagdollFromPlayerImpact(ped, false)
-                        SetEntityInvincible(ped, true)
-                    end
-
-                    if inputArgs.freezepos then
-                        FreezeEntityPosition(ped, true)
-                    end
-
-                    if IsPedInAnyVehicle(PlayerPedId(), false) and inputArgs.vehseat and inputArgs.vehseat ~= "" then
-                        SetPedIntoVehicle(ped, GetVehiclePedIsIn(PlayerPedId()), tonumber(inputArgs.vehseat))
-                    end
-
-                    table.insert(Peds, ped)
-                    QBCore.Functions.Notify("Created ped, the ped index is: " .. tostring(#Peds))
-                end
-            else
-                local hash = GetHashKey(tostring(commandArgs[1]))
-
+                local loadIdx = 0
                 RequestModel(hash)
-                while not HasModelLoaded(hash) do Wait(100) end
+                while not HasModelLoaded(hash) do 
+                    Wait(100) 
+                    loadIdx = loadIdx + 1
+                    if loadIdx > 20 then
+                        QBCore.Functions.Notify("Couldn't load / find the ped model", "error")
+                        return
+                    end
+                end
 
                 local ped = CreatePed(4, hash, coords.x, coords.y, coords.z - 1, 0.0, true, true)
 
                 SetEntityHeading(ped, GetEntityHeading(PlayerPedId()))
 
-                if IsPedInAnyVehicle(PlayerPedId(), false) and commandArgs[2] then
-                    SetPedIntoVehicle(ped, GetVehiclePedIsIn(PlayerPedId()), tonumber(commandArgs[2]))
+                if inputArgs.anim and Config.Animations[tonumber(inputArgs.anim)] and Config.Animations[tonumber(inputArgs.anim)].scenario  and (inputArgs.vehmodel == nil or inputArgs.vehmodel == "")then
+                    TaskStartScenarioInPlace(ped, Config.Animations[tonumber(inputArgs.anim)].scenario, 0, 1)
                 end
 
-                local scenario = nil
-
-                for i,v in pairs (Config.Animations) do
-                    if commandArgs[3] == v.value then
-                        scenario = v.scenario
-                    end
-                end
-
-                if scenario then
-                    TaskStartScenarioInPlace(ped, scenario, 0, 1)
-                end
-
-                if commandArgs[4] == "true" then
+                if inputArgs.invincible == "true" then
                     SetBlockingOfNonTemporaryEvents(ped, true)
                     SetPedDiesWhenInjured(ped, false)
                     SetPedCanPlayAmbientAnims(ped, true)
@@ -132,11 +96,35 @@ RegisterCommand("createped", function(source, commandArgs)
                     SetEntityInvincible(ped, true)
                 end
 
-                if commandArgs[5] == "true" then
+                if inputArgs.freezepos == "true" and (inputArgs.vehmodel == nil or inputArgs.vehmodel == "") then
                     FreezeEntityPosition(ped, true)
                 end
 
+                if inputArgs.followplr == "true" then
+                    if inputArgs.vehmodel == nil or inputArgs.vehmodel == "" then
+                        SetPedAsGroupMember(ped, GetPedGroupIndex(PlayerPedId()))
+                    else
+                        SetDriveTaskDrivingStyle(ped, 786468)
+                        SetDriverAbility(ped, 1.0)
+                        table.insert(FollowPlayerPeds, ped)
+                    end
+                end
+
+                if (inputArgs.vehmodel == nil or inputArgs.vehmodel == "") and IsPedInAnyVehicle(PlayerPedId(), false) and inputArgs.vehseat and inputArgs.vehseat ~= "" then
+                    SetPedIntoVehicle(ped, GetVehiclePedIsIn(PlayerPedId()), tonumber(inputArgs.vehseat))
+                end
+
                 table.insert(Peds, ped)
+
+                if inputArgs.vehmodel and inputArgs.vehmodel ~= "" then
+                    QBCore.Functions.SpawnVehicle(inputArgs.vehmodel, function(veh)
+                        if inputArgs.vehseat == "" or inputArgs.vehseat == nil then inputArgs.vehseat = "-1" end
+                        SetPedIntoVehicle(ped, veh, tonumber(inputArgs.vehseat))
+                        SetVehicleEngineOn(veh, true, false, true)
+                        Vehs[#Peds] = veh
+                    end, coords, true)
+                end
+
                 QBCore.Functions.Notify("Created ped, the ped index is: " .. tostring(#Peds))
             end
         else
@@ -152,6 +140,10 @@ RegisterCommand("removeped", function(source, args)
             DeletePed(Peds[tonumber(args[1])])
             Peds[tonumber(args[1])] = nil
         end
+        if Vehs[tonumber(args[1])] then
+            DeleteEntity(Vehs[tonumber(args[1])])
+            Vehs[tonumber(args[1])] = nil
+        end
 	end)
 end)	
 TriggerEvent('chat:addSuggestion', '/removeped', "Removes a created ped", {
@@ -166,6 +158,10 @@ RegisterCommand("clearpeds", function(source)
         for i,v in pairs(Peds) do
             DeletePed(v)
         end
+        for i,v in pairs(Vehs) do
+            DeleteEntity(v)
+        end
+        Vehs = {}
         Peds = {}
 	end)
 end)	
@@ -177,5 +173,21 @@ AddEventHandler("onResourceStop", function(resourceName)
         for i, v in pairs(Peds) do 
             DeletePed(v)
         end
+        for i,v in pairs(Vehs) do
+            DeleteEntity(v)
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        if #FollowPlayerPeds ~= 0 then
+            local PlayerPos = GetEntityCoords(PlayerPedId())
+            for i,ped in pairs(FollowPlayerPeds) do
+                local veh = GetVehiclePedIsIn(ped, false)
+                TaskVehicleDriveToCoord(ped, veh, PlayerPos.x,PlayerPos.y,PlayerPos.z, 20.0, 6.0, GetHashKey(veh), 1,0,10.0)
+            end
+        end
+        Citizen.Wait(1000)
     end
 end)
